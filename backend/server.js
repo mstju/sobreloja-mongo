ba9f05e5cf98da4812489cc89ccf8c9fc4b92cd5
Julia Sena <juliasenas@gmail.com>
2023-06-06 16:00:42 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const port = 4000;
-const MongoClient = mongodb.MongoClient;
 const uri = process.env.MONGO_URI;
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
